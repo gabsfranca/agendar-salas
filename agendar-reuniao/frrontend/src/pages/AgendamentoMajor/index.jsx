c1c08665887e43ef5ad7820458cc32b6d48d2416
Gabriel Menegasso de França <gabmenegasso@gmail.com>
2024-11-21 15:21:38 -0300

    front quase pronto

--- agendar-reuniao/frrontend/src/pages/AgendamentoMajor/index.jsx
+++ agendar-reuniao/frrontend/src/pages/AgendamentoMajor/index.jsx
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import Calendar from 'react-calendar';
-import 'react-calendar/dist/Calendar.css'; // Importar o estilo do calendário
+//import 'react-calendar/dist/Calendar.css'; // Importar o estilo do calendário
 import './styles.css'
 
 const AgendamentoMajor = () => {
